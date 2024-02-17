9e02be4446a782aa49575ab9146e12f825040eb4
grappphy <grappphy@gmail.com>
2024-02-17 19:40:17 +0900

    🔧 chore: remove lint singleQuote

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,8 +21,7 @@ module.exports = {
         'prettier/prettier': [
             'error',
             {
-                endOfLine: 'auto',
-                singleQuote: true
+                endOfLine: 'auto'
             }
         ]
     }
