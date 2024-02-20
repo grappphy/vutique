225293e4012db52058d9c9f4c040df1904a268a5
grappphy <grappphy@gmail.com>
2024-02-20 20:36:22 +0900

    ✨ [추가] styles

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
 <template>
-    <div></div>
+    <div>
+        <vu-el-button :tag="'button'" :color="'primary'" :size="'sm'" :text="'버튼'" />
+    </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+// Elements
+import VuElButton from './components/elements/button/el-button.vue';
+
+export default defineComponent({
+    components: {
+        VuElButton
+    }
+});
+</script>
+
+<style lang="scss">
+@import 'styles';
+</style>
