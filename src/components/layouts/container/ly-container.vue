d46b3bc7963ea91d5fffcc4d230fbd2415fc83f8
grappphy <grappphy@gmail.com>
2024-02-21 20:34:28 +0900

    ✨ add: ly-container

---
+++ src/components/layouts/container/ly-container.vue
@@ -0,0 +1,17 @@
+<template>
+    <span class="vu-container">
+        <slot />
+    </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'vu-ly-container'
+});
+</script>
+
+<style lang="scss">
+@import '../../../styles/layout/containers';
+</style>
