48becab54ca98acfef9fa6928439cf1206f99dce
grappphy <grappphy@gmail.com>
2024-03-22 19:35:41 +0900

    ✨ feat: el-stroke-rect

---
+++ src/components/elements/stroke/el-stroke-rect.vue
@@ -0,0 +1,20 @@
+<template>
+    <rect class="vu-stroke-rect" fill="transparent" width="100%" :stroke="stroke" :rx="999" :ry="999" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        /**
+         * 스트로크
+         *
+         * @type {String}
+         */
+        stroke: {
+            type: String
+        }
+    }
+});
+</script>
--- src/components/elements/stroke/index.ts
+++ src/components/elements/stroke/index.ts
@@ -1,1 +1,2 @@
 export { default as VuElStroke } from './el-stroke.vue';
+export { default as VuElStrokeRect } from './el-stroke-rect.vue';
