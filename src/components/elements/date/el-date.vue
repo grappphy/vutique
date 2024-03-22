1ec1ef2e426b38bc29608907a07266ca31a3f300
grappphy <grappphy@gmail.com>
2024-03-22 19:53:16 +0900

    ✨ feat: el-date

---
+++ src/components/elements/date/el-date.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="vu-date">
+        <!-- 슬롯: 텍스트 -->
+        <slot name="text" :default-class="'vu-date-text'" />
+
+        <!-- 슬롯: 툴팁 -->
+        <slot name="tooltip" :default-class="'vu-date-tooltip'" />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'vu-el-date'
+});
+</script>
+
+<style lang="scss">
+@import '../../../styles/components/date';
+</style>
---
+++ src/components/elements/date/index.ts
@@ -0,0 +1,1 @@
+export { default as VuElDate } from './el-date.vue';
